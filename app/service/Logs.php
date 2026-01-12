6602d97dff74cbf34154593e6881e44e9777c417
webmis <klingsoul@163.com>
2026-01-12 14:21:51 +0800

    用户消息

---
+++ app/service/Logs.php
@@ -0,0 +1,23 @@
+<?php
+namespace Service;
+
+use COre\Redis;
+use App\Librarys\FileEo;
+
+/* 日志 */
+class Logs {
+
+  /* 文件 */
+  static function File(string $file='', $content=''){
+    FileEo::WriterEnd($file, json_encode($content)."\n");
+  }
+
+  /* 商品-日志 */
+  static function Goods(array $data) {
+    $data['pdate'] = date('Y-m-d');
+    $redis = new Redis();
+    $redis->RPush('logs_goods', json_encode($data));
+    $redis->Close();
+  }
+
+}
