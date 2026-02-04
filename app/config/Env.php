2ce9e97d4c14734bd87e61f43afca26582c21fab
webmis <klingsoul@163.com>
2026-02-04 14:10:22 +0800

    JWT

--- app/config/Env.php
+++ app/config/Env.php
@@ -7,7 +7,7 @@ class Env {
   static $mode_cli = 'dev';                              // 开发环境: dev
   static $title = 'WebMIS 3.0';                          // 项目名称
   static $copy = 'webmis.vip &copy; 2026';               // 版权
-  static $key = 'e4b99adec618e653400966be536c45f8';      // KEY
+  static $key = 'e4b99adec618e653400966be536c45f8';      // 加密密钥
   static $password = '123456';                           // 默认密码
   // 资源
   static $img_url = 'https://php.webmis.vip/';           // 图片目录
--- app/librarys/Safety.php
+++ app/librarys/Safety.php
@@ -8,20 +8,26 @@ use \Firebase\JWT\JWT;
 class Safety {
 
   /* 正则-公共 */
-  static function IsRight(string $name='',string $val=''): bool {
-    $data = [
-      'uname'=>'/^[a-zA-Z][a-zA-Z0-9\_\@\-\*\&]{3,15}$/',
-      'passwd'=>'/^[a-zA-Z0-9|_|@|-|*|&]{6,16}$/',
-      'tel'=>'/^1\d{10}$/',
-      'email'=>'/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/',
-      'idcard'=>'/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/',
-    ];
-    return preg_match($data[$name],$val)?true:false;
+  static function IsRight(string $name='',string $value=''): bool {
+    switch ($name) {
+      case 'uname':
+        return self::Test('^[a-zA-Z][a-zA-Z0-9\_\@\-\*\&]{3,15}$', $value);
+      case 'passwd':
+        return self::Test('^[a-zA-Z0-9|_|@|-|*|&]{6,16}$', $value);
+      case 'tel':
+        return self::Test('^1\d{10}$', $value);
+      case 'email':
+        return self::Test('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', $value);
+      case 'idcard':
+        return self::Test('^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$', $value);
+      default:
+        return false;
+    }
   }
 
   /* 正则-验证 */
-  static function Test(string $reg, $val): bool {
-    return preg_match('/'.$reg.'/',$val)?true:false;
+  static function Test(string $reg, $value): bool {
+    return preg_match('/'.$reg.'/', $value)?true:false;
   }
 
   /* Base64-加密 */
